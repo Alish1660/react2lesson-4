52f0890bece5b8ae19e66cf458eed1dca0419396
Alish1660 <git>
2024-06-24 14:31:55 +0500

    toast

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -3,6 +3,8 @@ import TextField from "@mui/material/TextField";
 import { useState } from "react";
 import { auth } from "@service";
 import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Index = () => {
   const [form, setForm] = useState({});
@@ -14,19 +16,24 @@ const Index = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(form);
     try {
       const response = await auth.sign_in(form);
       if (response.status === 200) {
         localStorage.setItem("access_token", response?.data.access_token);
+        toast.success("Successfully logged in!");
+      } else {
+        toast.error("Access denied. Please check your credentials.");
       }
     } catch (error) {
       console.log(error);
+      toast.error("Failed to log in. Please try again later.");
     }
   };
 
   return (
     <>
+      <ToastContainer />
+
       <div className="w-full h-screen flex items-center justify-center">
         <div className="w-full sm:w-[600px] p-5">
           <h1 className="text-center my-6 text-[50px]">Login</h1>
